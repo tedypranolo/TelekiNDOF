a9d7e233b5da167c9a56a8351328c7e017bd6bdc
Tedy Pranolo <tedy.pranolo@gmail.com>
2023-08-26 22:32:54 +0900

    cleaned up space_utils.py

--- space_utils.py
+++ space_utils.py
@@ -1,31 +1,23 @@
 import bpy
 from bpy_types import PoseBone
-from mathutils import Quaternion, Matrix
+from mathutils import Quaternion, Matrix, Vector
 from typing import Callable
 
 from . import utils
+from utils import PoseBoneOrObject
 
 
-def apply_translation(target, world_delta_trans):
-    if isinstance(target, bpy.types.PoseBone):
-        def add_translation(matrix: Matrix):
-            matrix.translation += world_delta_trans
-            return matrix
-        apply_world_matrix_to_bone(target, add_translation)
-
-    else:
-        target.matrix_world.translation += world_delta_trans
+def apply_rotation_in_view(target: PoseBoneOrObject, view_delta_quat):
+    world_delta_quat = convert_rot_view_to_world(view_delta_quat.to_matrix())
+    apply_rotation(target, world_delta_quat)
 
 
-def convert_rot_active_view_to_world(view_quat: Quaternion) -> Quaternion:
-    view_rot = utils.get_active_region3d()
-    return view_rot @ view_quat @ view_rot.inverted()
+def apply_translation_in_view(target: PoseBoneOrObject, view_delta_trans):
+    world_delta_trans = convert_trans_view_to_world(view_delta_trans)
+    apply_translation(target, world_delta_trans)
 
 
-def apply_rotation_in_view_space(target, view_rot, view_delta_quat):
-    world_delta_quat = convert_rot_active_view_to_world(view_delta_quat.to_matrix())
-
-    # Check if the target is a pose bone
+def apply_rotation(target, world_delta_quat):
     if utils.is_pose_bone(target):
         def add_rotation(matrix: Matrix):
             loc, rot, scale = matrix.decompose()
@@ -36,6 +28,28 @@ def apply_rotation_in_view_space(target, view_rot, view_delta_quat):
     else:
         target.rotation_mode = "QUATERNION"
         target.rotation_quaternion = world_delta_quat @ target.rotation_quaternion
+
+
+def apply_translation(target, world_delta_trans):
+    if isinstance(target, bpy.types.PoseBone):
+        def add_translation(matrix: Matrix):
+            matrix.translation += world_delta_trans
+            return matrix
+
+        apply_world_matrix_to_bone(target, add_translation)
+
+    else:
+        target.matrix_world.translation += world_delta_trans
+
+
+def convert_rot_view_to_world(view_quat: Quaternion) -> Quaternion:
+    view_rot = utils.get_active_region3d()
+    return view_rot @ view_quat @ view_rot.inverted()
+
+
+def convert_trans_view_to_world(view_trans: Vector) -> Vector:
+    view_rot = utils.get_active_region3d()
+    return view_rot @ view_trans
 
 
 def apply_world_matrix_to_bone(pose_bone: PoseBone, func: Callable[[Matrix], None]) -> None:
